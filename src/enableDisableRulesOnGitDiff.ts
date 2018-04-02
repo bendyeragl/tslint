23798cc76f32946410b4e968cf001ee56dcbef1a
Ben Dyer <bdyer@agl.com.au>
2018-04-02 20:16:23 +1000

    first cut, no cli, with lint error for test

--- src/enableDisableRulesOnGitDiff.ts
+++ src/enableDisableRulesOnGitDiff.ts
@@ -21,12 +21,48 @@ import { execSync } from "child_process";
 import * as ts from "typescript";
 import { RuleFailure } from "./language/rule/rule";
 
-const includeMap = new Map<string, ts.TextRange[]>();
+let includeMap: Map<string, ts.TextRange[]>;
+const outFileNameRegex = /^\+\+\+ b(.*)$/m;
+const changeRegex = /^@@ \-\d+(?:,\d+)? \+(\d+)(?:,(\d+))? @@/mg;
 
 (function init() {
-    const result = execSync("git --no-pager diff -U0 origin/master");
-    console.log(result.toString('utf8')); //tslint:disable-line
+    const result = execSync("git --no-pager diff -U0 origin/master", {encoding: "utf8"});
+    const gitOutputByFile = splitGitOutputByFile(result);
+    const filtered = gitOutputByFile.filter((x) => x !== "");
+    const mapToBlob =  filtered.map(toFileNameMap)
+        .filter((x) => x !== null)
+        .map<[string, string]>((x) => x as [string, string]);
+    includeMap = mapToBlob.map(toChanges).reduce(
+        (accumulator: Map<string, ts.TextRange[]>, currentValue: [string, ts.TextRange[]]) => {
+            accumulator.set(currentValue[0], currentValue[1]);
+            return accumulator;
+        },
+        new Map<string, ts.TextRange[]>());
 })();
+
+function splitGitOutputByFile(input: string): string[] {
+    return input.split(/^diff --git a\/.* b\/.*$/m);
+}
+
+function toFileNameMap(fileBlob: string): [string, string] | null {
+    const outFileNameMatches = outFileNameRegex.exec(fileBlob);
+    if (outFileNameMatches === null) { return null; }
+    return [outFileNameMatches[1], fileBlob];
+}
+
+function toChanges(input: [string, string]): [string, ts.TextRange[]] {
+    let arr;
+    const range: ts.TextRange[] = [];
+    while((arr = changeRegex.exec(input[1])) !== null) { 
+        const len = arr[2] === undefined ? 1 : parseInt(arr[2], 10);
+        const pos = parseInt(arr[1], 10);
+        if (len > 0) {
+            const end = pos + len - 1;
+            range.push({pos, end});
+        }
+    }
+    return [input[0], range];
+}
 
 export function removeIrreleventFailures(sourceFile: ts.SourceFile, failures: RuleFailure[]): RuleFailure[] {
     if (failures.length === 0) {
